20947767846118c225fea72c3d169f73c1c375ef
GAYATHIRI <gayathiribala15@gmail.com>
2025-05-29 19:25:07 +0530

    Update RangePrimeNo.java

--- RangePrimeNo.java
+++ RangePrimeNo.java
@@ -1,16 +1,33 @@
  import java.util.Scanner;
- public class RangePrimeNo {
-    public static void main(String []args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the range of numbers for printing the Prime Numbers:");
-        System.out.println("Enter the starting value:");
-        int initial= sc.nextInt();
-         System.out.println("Enter the ending value:");
-        int end=sc.nextInt();
-        int flag=0;
-         
+
+public class RangePrimeNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the initial value:");
+        int initial = sc.nextInt();
+        System.out.println("Enter the ending value:");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers in the given range:");
+        for (int i = initial; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
     }
-     
 
-
- }    
+    // Corrected prime check method
+    static boolean isPrime(int i) {
+        if (i < 2) {
+            return false;
+        }
+        for (int j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
