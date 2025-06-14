11847956bbcce3bb7c8ecf8200041c272a902372
GAYATHIRI <gayathiribala15@gmail.com>
2025-06-14 11:17:42 +0530

    Add files via upload

---
+++ ArmstrongNo.java
@@ -0,0 +1,16 @@
+/*Armstrong No= It is defined as the special no,in which sum of each digit raised to the power of the number of digits is equal to the number itself.” */
+/* 153=  count=3  
+  1*1*1 + 5*5*5 + 3*3*3 => 1+125+27 =>153*/
+
+import java.util.Scanner;
+public class ArmstrongNo {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numbers:");
+        int number=sc.nextInt();
+        int oiginal = Math.abs(number);
+        int count=0;
+        if (number)
+   }
+    
+}
